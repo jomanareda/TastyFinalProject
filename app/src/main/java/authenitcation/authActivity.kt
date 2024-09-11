bcb644c7bdd61512b25ab657810e6d2829900f9b
jomanareda <145717237+jomanareda@users.noreply.github.com>
2024-09-12 00:53:31 +0300

    nav graph actions in fragments

--- app/src/main/java/authenitcation/authActivity.kt
+++ app/src/main/java/authenitcation/authActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.WindowManager
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,18 +17,23 @@ class authActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityAuthBinding
+    private  lateinit var  navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
 
-
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+// to navigate between fragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_auth) as NavHostFragment
+        navController = navHostFragment.navController
+
+
 
 //        setSupportActionBar(binding.toolbar)
 
@@ -41,9 +48,9 @@ class authActivity : AppCompatActivity() {
 //        }
     }
 
-//    override fun onSupportNavigateUp(): Boolean {
-//        val navController = findNavController(R.id.nav_host_fragment_auth)
-//        return navController.navigateUp(appBarConfiguration)
-//                || super.onSupportNavigateUp()
-//    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_auth)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
 }

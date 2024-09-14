396f9d4084fbffdacd8ca7e8e0778df5889bea50
Tassneemomar88 <tassneemomar880@gmail.com>
2024-09-14 13:02:39 +0300

    network

--- app/src/main/java/network/Meal.kt
+++ app/src/main/java/network/Meal.kt
@@ -3,5 +3,68 @@ package network
 data class Meal(
     val idMeal: String,
     val strMeal: String,
-    val strMealThumb: String
+    val strMealThumb: String,
+
+    val strInstructions: String,
+
+
+    val strIngredient1: String?,
+
+    val strIngredient2: String?,
+
+    val strIngredient3: String?,
+
+
+    val strIngredient4: String?,
+
+
+    val strIngredient5: String?,
+
+    val strIngredient6: String?,
+
+    val strIngredient7: String?,
+
+
+    val strIngredient8: String?,
+
+
+    val strIngredient9: String?,
+
+
+    val strIngredient10: String?,
+
+    val strIngredient11: String?,
+
+
+    val strMeasure1: String?,
+
+    val strMeasure2: String?,
+
+
+    val strMeasure3: String?,
+
+    val strMeasure4: String?,
+
+
+    val strMeasure5: String?,
+
+
+    val strMeasure6: String?,
+
+
+    val strMeasure7: String?,
+
+
+    val strMeasure8: String?,
+
+
+    val strMeasure9: String?,
+
+
+    val strMeasure10: String?,
+
+    val strMeasure11: String?,
+
+    val strYoutube: String?
+
 )

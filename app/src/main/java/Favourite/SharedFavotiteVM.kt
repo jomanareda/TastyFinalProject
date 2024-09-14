10b513dadcbf30fffe43c938bc056308c97369b9
Eslam Elhosuniy <119428009+eslamelhosuniy@users.noreply.github.com>
2024-09-14 12:15:06 +0300

    FavouriteFragment Done and add SharedFavotiteVM

---
+++ app/src/main/java/Favourite/SharedFavotiteVM.kt
@@ -0,0 +1,13 @@
+package Favourite
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SharedFavotiteVM: ViewModel() {
+    private val _selectedMealId = MutableLiveData<String>()
+    val selectedMealId: LiveData<String> get() = _selectedMealId
+
+    fun setSelectedMealId(mealId: String) {
+        _selectedMealId.value = mealId
+    }
+}
